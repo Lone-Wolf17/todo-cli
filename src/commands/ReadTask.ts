b8fd617a682133db0b05dd6c4e4669d4ade0e332
Korede <adeyanju.akorede1@gmail.com>
2023-09-03 16:36:17 +0100

    feat: filter and read todos by status

--- src/commands/ReadTask.ts
+++ src/commands/ReadTask.ts
@@ -2,25 +2,54 @@ import chalk from 'chalk';
 import ora from 'ora';
 
 import {connectDB, disconnectDB} from "../db/connect-db.js";
-import {TodoModel} from "../models/Todo.model.js";
+import { TodoModel} from "../models/Todo.model.js";
+import inquirer from "inquirer";
+import {Status} from "../constants/enums.js";
 
+const optionAll = "all";
+const askStatusFilterQuestion = async () => {
+    try {
+        // prompt the user to update the todo data
+        const response = await inquirer.prompt([
+            {
+                name: 'filter',
+                message: 'Select Todo Status filter?',
+                type: 'list',
+                choices: [...Object.values(Status), optionAll],
+                default: optionAll
+            }
+        ]);
+
+        return response.filter;
+    } catch (err) {
+        console.log('Something went wrong... \n', err);
+    }
+}
 export  default  async function readTask () {
     try {
+
+        const statusFilter = await askStatusFilterQuestion();
+
+        let filter: {status?: Status} = {};
+
+        if (statusFilter != optionAll) {
+            filter.status = statusFilter;
+        }
         // connecting to the database
         await  connectDB();
 
         /// start the spinner
-        const spinner = ora('Fetching all Todo...').start();
+        const spinner = ora(`Fetching ${statusFilter} Todos...`).start();
 
         // fetching all the todos from the database
-        const todos = await TodoModel.find({});
+        const todos = await TodoModel.find(filter);
 
         // stopping the spinner
         spinner.stop();
 
         // check if todos exist or not
         if (todos.length === 0) {
-            console.log(chalk.blueBright('You do not have any tasks yet!' ));
+            console.log(chalk.blueBright(`You do not have any ${statusFilter !== optionAll && statusFilter} tasks yet!` ));
         } else {
             todos.forEach(todo => {
                 console.log(
