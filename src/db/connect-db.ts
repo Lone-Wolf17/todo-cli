14f3d49116af7ee84da07ade7b5649f2a7af02c5
Korede <adeyanju.akorede1@gmail.com>
2023-09-03 01:41:30 +0100

    chore: connecting to mongodb database

---
+++ src/db/connect-db.ts
@@ -0,0 +1,27 @@
+import mongoose from  'mongoose';
+import ora from  'ora';
+import chalk from 'chalk';
+import EnvVars from "../constants/EnvVars";
+
+
+export const  connectDB = async () => {
+    try {
+        const spinner = ora('Connecting to the database...').start();
+        await mongoose.connect(EnvVars.MONGO_URI);
+        spinner.stop();
+        console.log(chalk.greenBright('Successfully connected to database!!!'))
+    } catch (error) {
+        console.log(chalk.redBright('Error: '), error);
+        process.exit(1);
+    }
+}
+
+export async function disconnectDB () {
+    try {
+        await mongoose.disconnect();
+        console.log(chalk.greenBright('Disconnected from the database.'));
+    } catch (err) {
+        console.log(chalk.redBright('Error: '), err);
+        process.exit(1)
+    }
+}
